4f7550f6774472ccad5f401fe82bca06cee7c006
KaroukinStanislau <karoukinstanislau@gmail.com>
2017-03-22 10:46:49 +0300

    requested changes have been made
    2 spaces, correct division, define inputs and move outside callback

--- calculator/script.js
+++ calculator/script.js
@@ -1,28 +1,32 @@
 window.onload = function() {
 
-    var calc = document.getElementById('calc');
-    var operations = {
-        "+": function(a, b) {
-            return a + b;
-        },
-        "-": function(a, b) {
-            return a - b;
-        },
-        "*": function(a, b) {
-            return a * b;
-        },
-        "/": function(a, b) {
-            return a / b;
-        },
-        "%": function(a, b) {
-            return Math.floor(a / b);
-        }
-    };
-
-    calc.onclick = function() {
-        var a = parseFloat(aInput.value);
-        var b = parseFloat(bInput.value);
-        var op = document.getElementById('operation').value;
-        document.getElementById('result').innerHTML = operations[op](a, b);
+  var aInput = document.getElementById('aInput');
+  var bInput = document.getElementById('bInput');
+  var op = document.getElementById('operation');
+  var res = document.getElementById('result');
+  var calc = document.getElementById('calc');
+  var operations = {
+    "+": function(a, b) {
+      return a + b;
+    },
+    "-": function(a, b) {
+      return a - b;
+    },
+    "*": function(a, b) {
+      return a * b;
+    },
+    "/": function(a, b) {
+      return a / b;
+    },
+    "%": function(a, b) {
+      var c = a / b;
+      return c > 0 ? Math.floor(c) : Math.ceil(c);
     }
+  };
+
+  calc.onclick = function() {
+    var a = parseFloat(aInput.value);
+    var b = parseFloat(bInput.value);
+    res.innerHTML = operations[op.value](a, b);
+  }
 }
