31dcabd075fe846c7a7a31b892414ee4bd29cf24
KaroukinStanislau <karoukinstanislau@gmail.com>
2017-04-05 20:17:02 +0300

    Rename method "recursive" to "binarySearch"

--- search/script.js
+++ search/script.js
@@ -1,12 +1,12 @@
 var a = [0, 1, 2, 2, 3, 4, 11, 22, 69, 235, 459, 637];
 
-console.log(recursive(a, 235));
-console.log(recursive(a, 0));
-console.log(recursive(a, 854));
-console.log(recursive(a, -854));
-console.log(recursive([], 0));
+console.log(binarySearch(a, 235));
+console.log(binarySearch(a, 0));
+console.log(binarySearch(a, 854));
+console.log(binarySearch(a, -854));
+console.log(binarySearch([], 0));
 
-function recursive(array, value, left, right) {
+function binarySearch(array, value, left, right) {
   if (typeof left === 'undefined' || typeof right === 'undefined') {
     left = 0;
     right = array.length;
@@ -18,8 +18,8 @@ function recursive(array, value, left, right) {
   if (array[middle] === value) {
     return middle;
   } else if (array[middle] > value) {
-    return recursive(array, value, left, middle - 1);
+    return binarySearch(array, value, left, middle - 1);
   } else {
-    return recursive(array, value, middle + 1, right);
+    return binarySearch(array, value, middle + 1, right);
   }
 }
