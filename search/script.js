140fb63324025909ae3d11fb0c8975333f8d02a9
KaroukinStanislau <karoukinstanislau@gmail.com>
2017-04-05 15:18:54 +0300

    fix indents, remove helper

--- search/script.js
+++ search/script.js
@@ -1,26 +1,23 @@
-   var a = [0, 1, 2, 2, 3, 4, 11, 22, 69, 235, 459, 637, 789, 854];
-   a.sort(function(a, b) {
-     return a - b;
-   })
+var a = [0, 1, 2, 2, 3, 4, 11, 22, 69, 235, 459, 637, 789, 854];
 
-   console.log(search(a, 235));
-   console.log(search(a, 0));
-   console.log(search(a, 854));
+console.log(recursive(a, 235));
+console.log(recursive(a, 0));
+console.log(recursive(a, 854));
 
-   function search(array, value) {
-     return recursive(array, value, 0, array.length);
-   };
-
-   function recursive(array, value, left, right) {
-     if (left > right) {
-       return -1;
-     }
-     var middle = Math.floor((right + left) / 2);
-     if (array[middle] > value) {
-       return recursive(array, value, left, middle - 1);
-     } else if (array[middle] < value) {
-       return recursive(array, value, middle + 1, right);
-     } else {
-       return middle;
-     }
-   }
+function recursive(array, value, left, right) {
+  if (typeof left === 'undefined' || typeof right === 'undefined') {
+    left = 0;
+    right = array.length;
+  }
+  if (left > right) {
+    return -1;
+  }
+  var middle = Math.floor((right + left) / 2);
+  if (array[middle] > value) {
+    return recursive(array, value, left, middle - 1);
+  } else if (array[middle] < value) {
+    return recursive(array, value, middle + 1, right);
+  } else {
+    return middle;
+  }
+}
